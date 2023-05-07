4e0521afc81e7f3247e17318664124a7fd692308
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-05-07 10:55:05 +0600

    fetch data from the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [user, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user);
+  }
   return (
     <>
       <h1>User Management System</h1>
+      <h3>Numbers of Users: {users.length}</h3>
+      {/* form start */}
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      {/* form end */}
+      <div>
+        {
+          users.map(user =>
+            <p key={user.id}>
+              {
+                user.id
+              }
+              :
+              {
+                user.name
+              }
+              :
+              {
+                user.email
+              }
+            </p>)
+        }
+      </div>
     </>
   )
 }
